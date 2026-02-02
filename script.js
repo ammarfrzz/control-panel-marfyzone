e705bb004d072e4b3bfc07c095d3e1d5d99c066d
MARMAREE <ammarfayruz06@gmail.com>
2026-02-02 14:08:38 +0700

    refactor: script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let btnRegister = document.getElementById('btnRegister');
 let loginForm = document.getElementById('loginForm');
 let registerForm = document.getElementById('registerForm');
 
-  btnLogin.addEventListener('click', () => {
+  btnLogin.addEventListener('click', function() {
     btnLogin.classList.add('active');
     btnRegister.classList.remove('active');
     loginForm.classList.add('active');
     registerForm.classList.remove('active');
   });
 
-  btnRegister.addEventListener('click', () => {
+  btnRegister.addEventListener('click', function() {
     btnRegister.classList.add('active');
     btnLogin.classList.remove('active');
     registerForm.classList.add('active');
